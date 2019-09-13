c90943d5ca481fa28f4a2ef2f44d706dcdfe1a72
John McCane-Whitney <john@espians.com>
2019-09-13 12:02:21 +0100

    new asf-site

---
+++ cdocs/search/variables_f.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['s',['S',['../structsha3.html#aa755ca9fd19e01f6181c7833188cb48e',1,'sha3']]],
+  ['statex',['stateX',['../structgcm.html#a6d96d37eb27ff5d03ab977473f157b21',1,'gcm']]],
+  ['status',['status',['../structgcm.html#ad41a374b5dcb99633c068765d1f801ee',1,'gcm']]]
+];
